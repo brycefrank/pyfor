07938a73e64f57cb4c01d6e7603e8412f731a0f8
brycefrank <bryce.frank@oregonstate.edu>
2017-02-28 12:53:55 -0800

    Setting up the setup.

---
+++ pyfor/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1'
+
+from PyFor.pyfor import gisexport
+from PyFor.pyfor import metrics
+from PyFor.pyfor import normalize
+from PyFor.pyfor import pointcloud
+from PyFor.pyfor import sampler
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PyFor',
+    version='0.1',
+    author='Bryce Frank',
+    author_email='bryce.frank@oregonstate.edu',
+    packages=['pyfor', 'pyfor.test'],
+    url='https://github.com/brycefrank/pyfor',
+    license='LICENSE.txt',
+    description='Tools for forest resource LiDAR.',
+    install_requires=[]
+)
