b7bdcda4515efc3c49b870fd53ecba62f11cc685
frankbr <bryce.frank@oregonstate.edu>
2017-04-27 10:28:33 -0700

    minor RTD fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, "C:\\pyformaster\\pyformaster\\PyFor\\pyfor")
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.fulltoc']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib-fulltoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
