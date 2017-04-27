ebed3064bb17216559ea03d138e3de5ec81b83e5
frankbr <bryce.frank@oregonstate.edu>
2017-04-27 13:06:19 -0700

    minor RTD fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,21 @@ import sys
 sys.path.insert(0,os.path.abspath(os.path.join('..','..','pyfor'))+os.path.sep)
 
 
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['affine', 'gdal', 'ogr', 'scipy', 'numpy', 'pandas', 'json', 'itertools','laspy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
