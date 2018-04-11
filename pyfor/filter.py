linked up a few things for normalization, cloud normalization is close but still needs a fix, added some TODOs

pyfor/filter.py
@@ -121,7 +121,7 @@ def zhang(array, number_of_windows, dh_max, dh_0, c, grid):
     X, Y = np.mgrid[0:grid.m, 0:grid.n]
     C = np.where(np.isfinite(B) == True)
     vals = B[C[0], C[1]]
-    dem_array = griddata(np.stack((C[0], C[1]), axis = 1), vals, (X, Y))
+    dem_array = griddata(np.stack((C[0], C[1]), axis = 1), vals, (X, Y), method="nearest")
 
     return(dem_array)
 

pyfor/test.py
@@ -2,31 +2,28 @@ import importlib.machinery
 import numpy as np
 from importlib import reload
 import matplotlib.pyplot as plt
-pyfor = importlib.machinery.SourceFileLoader('pyfor','/home/bryce/Programming/pyfor/pyfor/__init__.py').load_module()
+pyfor = importlib.machinery.SourceFileLoader('pyfor','/home/bryce/Programming/PyFor/pyfor/__init__.py').load_module()
 reload(pyfor)
 
-pc = pyfor.cloud.Cloud("/home/bryce/Programming/pyfor/samples/data/small_test.las")
+pc = pyfor.cloud.Cloud("/home/bryce/Desktop/pyfor_test_data/PC_001.las")
+pc.normalize(0.5)
 
-pc_grid = pc.grid(0.3)
+pc_grid = pc.grid(0.5)
 pc_grid.plot("max", "count")
 
-
-A = pc_grid._interpolate("min", "z")
-pc_grid.ground_filter()
+norm_grid = pc_grid.normalize()
 
 
-empty = pc_grid.empty_cells
-empty_x, empty_y = empty[:,0], empty[:,1]
-A[empty_y, empty_x] = np.nan
-B = np.where(flg != 0, A, np.nan)
+import pandas as pd
+arr = pc_grid.ground_filter()
 
+df = pd.DataFrame(arr.array).stack().rename_axis(['bins_y', 'bins_x']).reset_index(name='val')
 
-X, Y = np.mgrid[0:pc_grid.n, 0:pc_grid.m]
-# This is where the data is
+pc_grid.data = pd.merge(pc_grid.data, df)
 
-C = np.where(np.isfinite(B) == True)
-vals = B[C[0], C[1]]
-from scipy.interpolate import griddata
-dem_array = griddata(np.stack((C[0], C[1]), axis = 1), vals, (X, Y))
-plt.matshow(dem_array)
+pc_grid.data['z'] = pc_grid.data['z'] - pc_grid.data['val']
+
+pc_grid.cells = pc_grid.data.groupby(['bins_x', 'bins_y'])
+
+plt.matshow(pc_grid._interpolate("max", "z"))
 plt.show()