708fa27133a4acc582bf6d5614d1274ef742772f
brycefrank <bryce.frank@oregonstate.edu>
2018-10-06 15:52:04 -0700

    Adding kraus and pfeifer 1998 ground filter (rough)

--- pyfor/filter.py
+++ pyfor/filter.py
@@ -2,8 +2,7 @@ import numpy as np
 from scipy.ndimage.morphology import grey_opening
 from scipy.interpolate import griddata
 
-def window_size(k):
-    b = 2
+def window_size(k, b):
     return(2 * k * b + 1)
 
 
@@ -23,6 +22,7 @@ def slope(elev_array, w_k, w_k_1):
 
     Returns the slope coefficient s for a given elev_aray and w_k
     """
+    print(dhmax(elev_array))
     return(dhmax(elev_array) / ((w_k - w_k_1) / 2))
 
 
@@ -40,11 +40,12 @@ def dht(elev_array, w_k, w_k_1, dh_0, dh_max, c):
     if w_k <= 3:
         return(dh_0)
     elif w_k > 3:
+        return(s * 2 * 2 * c + dh_0)
         return(s * (w_k - w_k_1) * c + dh_0)
     else:
         return(dh_max)
 
-def zhang(array, number_of_windows, dh_max, dh_0, c, grid, interp_method = "nearest"):
+def zhang(array, number_of_windows, dh_max, dh_0, c, grid, b = 2, interp_method = "nearest"):
     """
     Implements Zhang et. al (2003), a progressive morphological ground filter. This returns a matrix of Z values for
     each grid cell that have been determined to be actual ground cells.
@@ -58,33 +59,35 @@ def zhang(array, number_of_windows, dh_max, dh_0, c, grid, interp_method = "near
     :param grid: The grid object used to construct the array
     :return: An array corresponding to the filtered points, can be used to construct a DEM via the Raster class
     """
-    w_k_list = list(map(window_size, range(number_of_windows)))
+
+    w_k_list = [window_size(i, b) for i in range(number_of_windows)]
     w_k_min = w_k_list[0]
     A = array
     m = A.shape[0]
     n = A.shape[1]
     flag = np.zeros((m, n))
-    for w_k in enumerate(w_k_list):
-        opened = grey_opening(array, (w_k[1], w_k[1]))
-        if w_k[1] == w_k_min:
+    for k, w_k in enumerate(w_k_list):
+        opened = grey_opening(array, (w_k, w_k))
+        if w_k == w_k_min:
             w_k_1 = 0
         else:
-            w_k_1 = w_k_list[w_k[0] - 1]
+            w_k_1 = w_k_list[k - 1]
         for i in range(0, m):
             P_i = A[i,:]
             Z = P_i
             Z_f = opened[i,:]
-            dh_t = dht(Z, w_k[1], w_k_1, dh_0, dh_max, c)
+            dh_t = dht(Z, w_k, w_k_1, dh_0, dh_max, c)
             for j in range(0, n):
+                print(Z[j] - Z_f[j], dh_t)
                 if Z[j] - Z_f[j] > dh_t:
-                    flag[i, j] = w_k[1]
+                    flag[i, j] = w_k
             P_i = Z_f
             A[i,:] = P_i
 
     if np.sum(flag) == 0:
         print("No ground points classified.")
         return(None)
-
+    return(flag)
     # Remove interpolated cells
     empty = grid.empty_cells
     empty_y, empty_x = empty[:,0], empty[:,1]
@@ -98,4 +101,29 @@ def zhang(array, number_of_windows, dh_max, dh_0, c, grid, interp_method = "near
     dem_array = griddata(np.stack((C[0], C[1]), axis = 1), vals, (X, Y), method=interp_method)
 
     return(dem_array)
+
+def compute_weights(v_i, a, b, g, w):
+    p_i = np.empty(v_i.shape)
+    p_i[v_i <= g] = 1
+    p_i[np.logical_and(v_i > g, v_i <= g+w)] = 1 / (1 + (a * (v_i[np.logical_and(v_i > g, v_i <= g+w)] - g)**b))
+    p_i[v_i > g+w] = 0
+    return p_i
+
+def kraus_and_pfeifer(cloud, cell_size, a = 1, b = 4, g = -2, w = 2.5, iterations = 5):
+    import pandas as pd
+    grid = cloud.grid(cell_size)
+    for i in range(iterations):
+        # Calculate the surface from previous z values
+        mean_surface = grid.raster(np.mean, 'z')
+        df = pd.DataFrame(mean_surface.array).stack().rename_axis(['bins_y', 'bins_x']).reset_index(name='val')
+        df = cloud.las.points.reset_index().merge(df, how = "left").set_index('index')
+
+        # Calculate residuals
+        df['v_i'] = df['z'] - df['val']
+        v_i = df['v_i'].values
+
+        p_i = compute_weights(v_i, a, b, g, w)
+
+        # Modify z values
+        grid.data['z'] = df['z'] * p_i
 
