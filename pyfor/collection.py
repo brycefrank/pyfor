d2efdd65d378192849ea7da90c4beafbd66f46d7
brycefrank <bryce.frank@oregonstate.edu>
2019-03-08 20:45:27 -0800

    A lot of fancy-free ideas for `CloudDataFrame`, getting things going for `Retiler`. This will surely break things, but I would like to go watch Deep Space Nine now.

--- pyfor/collection.py
+++ pyfor/collection.py
@@ -7,7 +7,6 @@ import pandas as pd
 import laxpy
 import pyproj
 
-
 class CloudDataFrame(gpd.GeoDataFrame):
     """
     Implements a data frame structure for processing and managing multiple :class:`.Cloud` objects. It is recommended \
@@ -16,9 +15,11 @@ class CloudDataFrame(gpd.GeoDataFrame):
     def __init__(self, *args, **kwargs):
         super(CloudDataFrame, self).__init__(*args, **kwargs)
         self.n_threads = 1
+        self.tiles = None
 
         if "bounding_box" in self.columns.values:
             self.set_geometry("bounding_box", inplace=True)
+            self.tiles = self['bounding_box'].values
 
     @classmethod
     def _from_dir(cls, las_dir, n_jobs=1, get_bounding_boxes=True):
@@ -44,14 +45,14 @@ class CloudDataFrame(gpd.GeoDataFrame):
     def set_index(self, *args):
         return CloudDataFrame(super(CloudDataFrame, self).set_index(*args))
 
-    def par_apply(self, func, column='las_path', buffer_distance=0, *args):
+    def par_apply(self, func, *args):
         """
-        Apply a function to each item in `column`. Allows for parallelization using the n_jobs argument. This is \
-        achieved via :class:`joblib.Parallel` and :func:`joblib.delayed`.
+        Apply a function to the point cloud described by each tile in `self.tiles` such that the first argument of the
+        function is a :class:`pyfor.cloud.Cloud` object. This is achieved via :class:`joblib.Parallel` and :func:`joblib.delayed`.
 
-        :param func: The user defined function, must accept as an argument the value of each row of `column`.
-        :param column: The column to apply on, will be the first argument to func.
-        :param buffer_distance: The distance to buffer and aggregate each tile.
+        Interested in applying a function to buffered point clouds? That should be implemented with a retiling operation.
+
+        :param func: The user defined function, must accept as its first argument a :class`pyfor.cloud.Cloud` object.
         :param *args: Further arguments to `func`
         """
         from joblib import Parallel, delayed
@@ -68,6 +69,25 @@ class CloudDataFrame(gpd.GeoDataFrame):
 
         output = Parallel(n_jobs=self.n_threads)(delayed(func)(plot_path, *args) for plot_path in self[column])
         return output
+
+    def retile_buffer(self):
+        """
+        A basic retiling operation that buffers the current `self.tiles` using a square buffer.
+        :return:
+        """
+
+        pass
+
+    def retile_raster(self, cell_size, original_tile_size, buffer=0):
+        """
+        A retiling operation that creates raster-compatible sized tiles. Important for creating project-level rasters.
+        Changes `self.tiles` **in place**
+
+        :param buffer: Buffer the raster-compatible tiles.
+        """
+
+        retiler = Retiler(self)
+        self.tiles = retiler.retile_raster(cell_size, original_tile_size, buffer)
 
     def _get_bounding_box(self, las_path):
         """
@@ -88,6 +108,7 @@ class CloudDataFrame(gpd.GeoDataFrame):
         self["bounding_box"] = [Polygon(((bbox[0], bbox[2]), (bbox[1], bbox[2]),
                                            (bbox[1], bbox[3]), (bbox[0], bbox[3]))) for bbox in bboxes]
         self.set_geometry("bounding_box", inplace = True)
+        self.tiles = self['geometry'].values
 
     def _get_intersecting(self, tile_index):
         """
@@ -136,37 +157,6 @@ class CloudDataFrame(gpd.GeoDataFrame):
         col_bbox = np.min(minx), np.min(miny), np.max(maxx), np.max(maxy)
         return col_bbox
 
-    def retile(self, out_dir, verbose = False):
-        """
-        Retiles the collection into `out_dir`. This is a simplified retiling function that splits each tile into \
-        quadrants and writes these new quadrants to disk. The written files will be the original file name with an index
-        appended. 0 is the bottom left quadrant, 1 is the bottom right, 2 is the top left and 3 is the top right.
-        """
-        from shapely.geometry import Polygon
-
-        for index, row in self.iterrows():
-            # Build the quadrant geometries, this is defined by the following six values
-            larger_cell = row['bounding_box'].bounds
-
-            x0, y0 = larger_cell[0], larger_cell[1]
-            x2, y2 = larger_cell[2], larger_cell[3]
-            x1 = ((larger_cell[2] - larger_cell[0]) / 2) + x0
-            y1 = ((larger_cell[3] - larger_cell[1]) / 2) + y0
-
-            # Create the geometries
-            bottom_left = Polygon([(x0, y0), (x0, y1), (x1, y1), (x1, y0)])
-            bottom_right = Polygon([(x1, y0), (x1, y1), (x2, y1), (x2, y0)])
-            top_left = Polygon([(x0, y1), (x0, y2), (x1, y2), (x1, y1)])
-            top_right = Polygon([(x1, y1), (x1, y2), (x2, y2), (x2, y1)])
-
-            quadrants = [bottom_left, bottom_right, top_left, top_right]
-
-
-            larger_cloud = pyfor.cloud.Cloud(row['las_path'])
-
-            for i, quad in enumerate(quadrants):
-                clipped = larger_cloud.clip(quad)
-                clipped.write(os.path.join(out_dir, '{}_{}.las'.format(larger_cloud.name, i)))
 
     def _retile2(self, width, height, dir):
         """
@@ -239,20 +229,6 @@ class CloudDataFrame(gpd.GeoDataFrame):
 
             func(first, *args)
 
-    def _get_parents(self, polygon):
-        return self[self['bounding_box'].intersects(polygon)]
-
-    def _square_buffer(self, polygon, buffer):
-        from shapely.geometry import Polygon
-        minx, miny, maxx, maxy = polygon.bounds
-        n_minx, n_miny, n_maxx, n_maxy = minx - buffer, miny - buffer, maxx + buffer, maxy + buffer
-
-        buffered_poly = Polygon([[n_minx, n_miny],
-                                 [n_minx, n_maxy],
-                                 [n_maxx, n_maxy],
-                                 [n_maxx, n_miny]])
-        return buffered_poly
-
     def _clip_no_index(self, polygons, func):
         # TODO clean this function, parallelize, etc.
         """
@@ -272,54 +248,6 @@ class CloudDataFrame(gpd.GeoDataFrame):
                 parents[i] = poly_parents
 
         Parallel(n_jobs=self.n_threads)(delayed(self._merge_parents)(parent_list, func, [polygons[poly_index], poly_index]) for poly_index, parent_list in parents.items())
-
-    def _retiling_grid(self, target_cell_size, original_tile_size, buffer = 0):
-        """
-        Creates a retiling grid for a specified target cell size. This creates a list of polygons such that if a raster
-        is constructed from a polygon it will exactly fit inside given the specified target cell size. Useful for creating
-        project level rasters.
-
-        :param target_cell_size: The desired output cell size
-        :param original_tile_size: The original tile size of the project
-        :param buffer: The distance to buffer each new tile to prevent edge effects.
-        :return: A list of shapely polygons that correspond to the new grid.
-        """
-        from shapely.geometry import Polygon
-
-        bottom, left = self.bounding_box[1], self.bounding_box[0]
-        top, right = self.bounding_box[3], self.bounding_box[2]
-
-        new_tile_size = np.ceil(original_tile_size / target_cell_size) * target_cell_size
-        extension = new_tile_size - original_tile_size
-
-        project_width = right - left
-        project_height = top - bottom
-
-        num_x = int(np.ceil(project_width / new_tile_size))
-        num_y = int(np.ceil(project_height / new_tile_size))
-
-        new_tiles = []
-        for i in range(num_x):
-            for j in range(num_y):
-                # Create geometry
-                tile_left, tile_bottom = left + i * new_tile_size, bottom + j * new_tile_size
-
-                new_tile = Polygon([
-                    [tile_left, tile_bottom], #bl
-                    [tile_left, tile_bottom + new_tile_size], #tl
-                    [tile_left + new_tile_size, tile_bottom + new_tile_size], #tr
-                    [tile_left + new_tile_size, tile_bottom]]) #br
-
-                if buffer > 0:
-                    new_tile = self._square_buffer(new_tile, buffer)
-
-                # Only append if there are any original tiles touching
-                if len(self._get_parents(new_tile)) > 0:
-                    new_tiles.append(new_tile)
-
-        return new_tiles
-
-
 
     def _clip_no_index1(self, polygons, func):
         # TODO clean this function, parallelize, etc.
@@ -475,7 +403,120 @@ class CloudDataFrame(gpd.GeoDataFrame):
 
         return metrics
 
+class Retiler:
 
+    def __init__(self, cdf):
+        """
+        Retiles a CloudDataFrame. Generally used to create tiles such that rasters generated from tiles are properly aligned.
+        """
+        from shapely.geometry import Polygon
+        self.cdf = cdf
+
+    def _get_parents(self, polygon):
+        """
+        For a given input polygon, finds the files whose bounding boxes intersect with that polygon.
+
+        :param polygon:
+        :return: A GeoDataFrame of intersecting file bounding boxes.
+        """
+        return self.cdf[self.cdf['bounding_box'].intersects(polygon)]
+
+    def _square_buffer(self, polygon, buffer):
+        """
+        A simple square buffer that expands a square by a given buffer distance.
+
+        :param polygon: A shapely polygon.
+        :param buffer: The buffer distance.
+        :return: A buffered shapely polgon
+        """
+
+        minx, miny, maxx, maxy = polygon.bounds
+        n_minx, n_miny, n_maxx, n_maxy = minx - buffer, miny - buffer, maxx + buffer, maxy + buffer
+
+        buffered_poly = Polygon([[n_minx, n_miny],
+                                 [n_minx, n_maxy],
+                                 [n_maxx, n_maxy],
+                                 [n_maxx, n_miny]])
+        return buffered_poly
+
+    def retile_raster(self, target_cell_size, original_tile_size, buffer = 0):
+        """
+        Creates a retiling grid for a specified target cell size. This creates a list of polygons such that if a raster
+        is constructed from a polygon it will exactly fit inside given the specified target cell size. Useful for creating
+        project level rasters.
+
+        :param target_cell_size: The desired output cell size
+        :param original_tile_size: The original tile size of the project
+        :param buffer: The distance to buffer each new tile to prevent edge effects.
+        :return: A list of shapely polygons that correspond to the new grid.
+        """
+
+        bottom, left = self.cdf.bounding_box[1], self.cdf.bounding_box[0]
+        top, right = self.cdf.bounding_box[3], self.cdf.bounding_box[2]
+
+        new_tile_size = np.ceil(original_tile_size / target_cell_size) * target_cell_size
+
+        project_width = right - left
+        project_height = top - bottom
+
+        num_x = int(np.ceil(project_width / new_tile_size))
+        num_y = int(np.ceil(project_height / new_tile_size))
+
+        new_tiles = []
+        for i in range(num_x):
+            for j in range(num_y):
+                # Create geometry
+                tile_left, tile_bottom = left + i * new_tile_size, bottom + j * new_tile_size
+
+                new_tile = Polygon([
+                    [tile_left, tile_bottom], #bl
+                    [tile_left, tile_bottom + new_tile_size], #tl
+                    [tile_left + new_tile_size, tile_bottom + new_tile_size], #tr
+                    [tile_left + new_tile_size, tile_bottom]]) #br
+
+                if buffer > 0:
+                    new_tile = self._square_buffer(new_tile, buffer)
+
+                # Only append if there are any original tiles touching
+                if len(self._get_parents(new_tile)) > 0:
+                    new_tiles.append(new_tile)
+
+        return new_tiles
+
+    def retile_buffer(self, tiles, buffer):
+        """
+        A simple buffering operation.
+
+        :return: A list of buffered shapely polygons.
+        """
+
+        return [self._square_buffer(tile, buffer) for tile in tiles]
+
+
+    def retile_quadrant(self, tiles):
+        """
+        Splits input tiles into quadrants. An efficient retiling method when particular retiling geometries are not
+        necessary.
+        """
+
+        new_tiles = []
+        for tile in tiles:
+            # Build the quadrant geometries, this is defined by the following six values
+            x0, y0 = tile[0], tile[1]
+            x2, y2 = tile[2], tile[3]
+            x1 = ((tile[2] - tile[0]) / 2) + x0
+            y1 = ((tile[3] - tile[1]) / 2) + y0
+
+            # Create the geometries
+            bottom_left = Polygon([(x0, y0), (x0, y1), (x1, y1), (x1, y0)])
+            bottom_right = Polygon([(x1, y0), (x1, y1), (x2, y1), (x2, y0)])
+            top_left = Polygon([(x0, y1), (x0, y2), (x1, y2), (x1, y1)])
+            top_right = Polygon([(x1, y1), (x1, y2), (x2, y2), (x2, y1)])
+
+            quadrants = [bottom_left, bottom_right, top_left, top_right]
+            new_tiles.append(quadrants)
+
+        return [quadrant for sublist in new_tiles for quadrant in sublist]
 
 def from_dir(las_dir, **kwargs):
     """
